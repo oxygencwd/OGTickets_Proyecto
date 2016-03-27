e6250f1eb4dcfb8459dcc9b57bd0f131d8250117
David Guillen <maiden8-8@hotmail.com>
2016-03-27 14:35:55 -0600

    Validacion y logica de inicio sesion

--- logic/controllers/loginController.js
+++ logic/controllers/loginController.js
@@ -1,6 +1,26 @@
 angular.module('OGTicketsApp.controllers')
 .controller('loginController', ['$scope', function ($scope) {
 	
-
+$scope.canLogIn= function () {
+	var saved= $scope.accountExists();
+	var loggedUser;
+	if(saved.length>0){
+		if(saved[0].username==$scope.username){
+			if(saved[0].pass==$scope.pass){
+				BDService.userLogIn(saved[0]);
+				loggedUser= BDService.getloggedUser();
+				$scope.userError="";
+				$scope.username="";
+				$scope.pass="";
+				$scope.clearForm();
+				$window.location.href = ('#/summary/' + loggedUser.id);
+			}else{
+				$scope.userError= "Usuario o contraseña invalidos";
+			}
+		}
+	}else{
+		$scope.userError= "Usuario o contraseña invalidos";
+	}
+};
 
 }]); //end -controller-
