91ff14efd494955216ff499d2f2792f16de8ae70
David Guillen <maiden8-8@hotmail.com>
2016-03-27 16:33:58 -0600

    Agregar controller en formulario

--- logic/controllers/loginController.js
+++ logic/controllers/loginController.js
@@ -1,26 +1,33 @@
 angular.module('OGTicketsApp.controllers')
 .controller('loginController', ['$scope', function ($scope) {
 	
-$scope.canLogIn= function () {
-	var saved= $scope.accountExists();
-	var loggedUser;
-	if(saved.length>0){
-		if(saved[0].username==$scope.username){
-			if(saved[0].pass==$scope.pass){
-				BDService.userLogIn(saved[0]);
-				loggedUser= BDService.getloggedUser();
-				$scope.userError="";
-				$scope.username="";
-				$scope.pass="";
-				$scope.clearForm();
-				$window.location.href = ('#/summary/' + loggedUser.id);
-			}else{
-				$scope.userError= "Usuario o contraseña invalidos";
+	$scope.canLogIn= function () {
+		var saved= $scope.accountExists();
+		var loggedUser;
+		if(saved.length>0){
+			if(saved[0].username==$scope.useremail){
+				if(saved[0].pass==$scope.pass){
+					BDService.userLogIn(saved[0]);
+					loggedUser= BDService.getloggedUser();
+					$scope.userError="";
+					$scope.useremail="";
+					$scope.pass="";
+					$scope.clearForm();
+					$window.location.href = ('#/summary/' + loggedUser.id);
+				}else{
+					$scope.userError= "Usuario o contraseña invalidos";
+				}
 			}
+		}else{
+			$scope.userError= "Usuario o contraseña invalidos";
 		}
-	}else{
-		$scope.userError= "Usuario o contraseña invalidos";
-	}
-};
+	};
+
+	$scope.clearForm= function () {
+			if ($scope.canLogIn) {
+                    $scope.canLogIn.$setPristine();
+                    $scope.canLogIn.$setUntouched();
+            }
+	};
 
 }]); //end -controller-
