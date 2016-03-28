9291de5eaff32c5ab2a76246d463603b396bcdc6
David Guillen <maiden8-8@hotmail.com>
2016-03-27 20:38:37 -0600

    Validacion y logica de registrar nuevo sitio

--- logic/controllers/siteRegistrationController.js
+++ logic/controllers/siteRegistrationController.js
@@ -1,6 +1,23 @@
 angular.module('OGTicketsApp.controllers')
 .controller('siteRegistrationController', ['$scope', function ($scope) {
 	
+	$scope.addTransact= function () {
+			var transacIds= BDService.getTransactCount();
+			$scope.newTransat.id= transacIds;
+			loggedUser.transac.push($scope.newTransat);
+			BDService.updateTransactCount();
+			$scope.msj= "Movimiento agregado con éxito";
+			clearForm();
+			BDService.updateLoggedUser();
+			$scope.newTransat={};
+			//$window.location.href = ('#/detail/' + $scope.userId+'/' + $scope.newTransat.id );
+		};
 
+	var	clearForm= function () {
+		if ($scope.addTransactForm) {
+               $scope.addTransactForm.$setPristine();
+                $scope.addTransactForm.$setUntouched();
+        }
+	}; //fin function
 
 }]); //end -controller-
