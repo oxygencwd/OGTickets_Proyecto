b84dc332ab6c4a32870032e2e65b3944b21614fe
David Guillen <maiden8-8@hotmail.com>
2016-03-29 11:18:49 -0600

    Actualizacion

--- logic/controllers/siteRegistrationController.js
+++ logic/controllers/siteRegistrationController.js
@@ -1,30 +1,6 @@
 angular.module('OGTicketsApp.controllers')
 .controller('siteRegistrationController', ['$scope', function ($scope) {
+	
 
-
-	$scope.newSite={};
-
-	$scope.addTransact= function () {
-			var transacIds= BDService.getTransactCount();
-			$scope.newSite.id= transacIds;
-			loggedUser.transac.push($scope.newSite);
-			BDService.updateTransactCount();
-			$scope.msj= "Sitio creado";
-			clearForm();
-			BDService.updateLoggedUser();
-			$scope.newSite={};
-			//$window.location.href = ('#/detail/' + $scope.userId+'/' + $scope.newSite.id );
-		};
-
-	$scope.expNumberPhone = /^[\ |0-9]{8}$/;
-	$scope.expDecimalLatitud = /^-?\d{0,2}(?:,\s?\d{3})*(?:\.\d*)?$/;
-	$scope.expDecimalLongitud = /^-?\d{0,3}(?:,\s?\d{3})*(?:\.\d*)?$/;
-
-	var	clearForm= function () {
-		if ($scope.addTransactForm) {
-               $scope.addTransactForm.$setPristine();
-                $scope.addTransactForm.$setUntouched();
-        }
-	}; //fin function
 
 }]); //end -controller-
