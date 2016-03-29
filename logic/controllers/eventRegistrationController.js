02fdbae39a6f98b7eca8f7536463c0cd05042a44
ariaguilar21 <ariaguiuma@hotmail.com>
2016-03-28 18:52:25 -0600

    creando el registration form

--- logic/controllers/eventRegistrationController.js
+++ logic/controllers/eventRegistrationController.js
@@ -1,6 +1,34 @@
 angular.module('OGTicketsApp.controllers')
 .controller('eventRegistrationController', ['$scope', function ($scope) {
-	
+
+	$scope.eventTypes=[
+		{
+			value: 'music',
+			name: 'Música'
+		},
+		{
+			value: 'drama',
+			name: 'Teatro'
+		},
+		{
+			value: 'culture',
+			name: 'Cultura'
+		},
+		{
+			value: 'sports',
+			name: 'Deportes'
+		},
+		{
+			value: 'art',
+			name: 'Arte'
+		},
+	]
+
+	$scope.singleEvent = {
+		name: '',
+		eventType: 'music',
+
+	}
 
 
 }]); //end -controller-
