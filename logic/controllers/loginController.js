c2576d5541157fdede64d61bd0ffc221c11cfb94
Natalie Mata <Natalie Mata>
2016-03-31 02:48:05 -0600

    validaciones del login

--- logic/controllers/loginController.js
+++ logic/controllers/loginController.js
@@ -1,5 +1,17 @@
 angular.module('OGTicketsApp.controllers')
 .controller('loginController', ['$scope', function ($scope) {
 
+	$scope.user={};
+
+	// $scope.login= function () {
+	// 	userService.canLogin(user);
+	// 	formService.clearForm($scope.loginForm);
+	// };
+
+	
+
+	$scope.closeModal= function () {	
+		$('#loginModal').modal('hide');
+	};
 
 }]); //end -controller-
--- logic/services/userService.js
+++ logic/services/userService.js
@@ -2,12 +2,17 @@ angular.module('OGTicketsApp.services')
 .service('userService', ['localStorageService', function(localStorageService) {
 
 	/* login, logout, register, isLoggedIn, getCurrentUser*/
-	
+	var canLogin= function () {
+		
+	};
+
+	var login= function () {
+		
+	};
     
 
 //puntos de acceso de los metodos del servicio:
 	return{
-		// metodo1:metodo1,
-  //       metodo2:metodo2
+		
 	};
 }]);//end -service-
