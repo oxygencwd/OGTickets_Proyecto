82e4b29f038dc27dfaf431088b8f12813129a690
David Guillen <maiden8-8@hotmail.com>
2016-04-02 02:58:11 -0600

    Actualizacion Validaciones y ngModel

---
+++ logic/datosQuemados.js
@@ -0,0 +1,16 @@
+var clientList=[{ "name": "Manuel Mendoza", "id": "cl01", "email": "manuelmendoza@gmail.com", "password": "Abcdefg1", "gender": "Masculino", "personalId": "115290295", "birthday": "1994-04-29T06:00:00.000Z", "userType": 2 },
+{ "name": "Juan Pérez", "id": "cl02", "email": "juanperez@gmail.com", "password": "Juanperez2", "gender": "Masculino", "personalId": "109820432", "birthday": "1984-06-22T06:00:00.000Z", "userType": 2 },
+{ "name": "Karla Jiménez", "id": "cl03", "email": "karlajimenez@yahoo.es", "password": "Karlita26", "gender": "Femenino", "personalId": "112980324", "birthday": "1990-06-26T06:00:00.000Z", "userType": 2 }];
+
+var promotorList=[{ "name": "Cheese Productions", "password": "Holahola77", "id": "pr01", "email": "cheeseproductions@gmail.com", "personalId": "3816497372", "areaOfSpecialization": "Conciertos", "phoneOne": "60324127", "address": "Desamparados, San José", "phoneTwo": "22236543", "userType": 3 },
+{ "name": "Jonathan Ryzowy", "password": "Distrito55", "email": "ryzowy@gmail.com", "personalId": "105720123", "areaOfSpecialization": "Cultura", "phoneOne": "88328319", "id": "pr02", "address": "Santa Ana, Costa Rica", "userType": 3 },
+{ "name": "Francisca Productions", "password": "Francisquita2", "id": "pr03", "email": "info@franciscaproductions.com", "personalId": "3692134812", "areaOfSpecialization": "Deportes", "phoneOne": "22138844", "address": "Santo Domingo, Heredia", "userType": 3 }]
+
+var cashierList=[{ "name": "Juanita Hidalgo Rodríguez", "password": "Hidalgo92", "id": "cs01", "gender": "Femenino", "email": "j.hidalgo.rodriguez@hotmail.com", "phone": "88120922", "birthday": "1991-10-21T06:00:00.000Z", "userType": 4 },
+{ "name": "Josefina Duarte", "password": "Duarte7621", "id": "cs02", "gender": "Femenino", "email": "josefita@yahoo.com", "phone": "22216709", "birthday": "1983-09-12T06:00:00.000Z", "userType": 4 },
+{ "name": "Carlos Ugalde", "password": "Ugalde2223", "id": "cs03", "gender": "Masculino", "email": "carlosugalde_8732@hotmail.com", "phone": "73902476", "birthday": "1993-01-29T06:00:00.000Z", "userType": 4 }];
+
+var eventsList=[{ "date": "2016-04-27T06:00:00.000Z", "name": "Rihanna World Tour", "ticketsPrice": 35500, "capacity": 6000 },
+{ "name": "Costa Rica vs Jamaica", "date": "2016-04-17T06:00:00.000Z", "ticketsPrice": 8000, "capacity": 7000 },
+{ "name": "Malpaís en concierto", "date": "2016-05-16T06:00:00.000Z", "ticketsPrice": 5000, "capacity": 3000 }];
+
