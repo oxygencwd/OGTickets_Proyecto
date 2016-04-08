9d1288da12e2e56d424fc0cfed58267d0f6d78a3
Natalie Mata <natymata@gmail.com>
2016-04-08 01:32:39 -0600

    segunda parte de las butacas y comras

--- logic/controllers/eventProfileController.js
+++ logic/controllers/eventProfileController.js
@@ -4,6 +4,7 @@ angular.module('OGTicketsApp.controllers')
 
         var eventId= $routeParams.eventId;
 
+        /*display event*/
         // Sets on "currentEvent" the whole event by the id
         $scope.currentEvent = eventService.retrieveEvent(eventId);
         var eventSite= siteService.getEventSite(eventId);
@@ -11,12 +12,12 @@ angular.module('OGTicketsApp.controllers')
         $scope.eventSiteId= eventSite.id;
         //noMap indica si el evento tiene un sitio con mapa o no
         $scope.noMap=false;
-
+        //ocultar los paneles de compra hasta que el cliente vaya avanzando hacia esa seccion.
         $scope.inputPanel= false;
         $scope.showMap= false;
         $scope.buttons= false;
         $scope.resume= false;
-        $scope.seatsDisplay= true; //esto cambiarlo a false y ponerlo en true en la vara de 
+        $scope.seatsDisplay= false; //esto cambiarlo a false y ponerlo en true en la vara de 
 
         if( $scope.eventSiteId!=='si01' &&  $scope.eventSiteId!=='si02' &&  $scope.eventSiteId!=='si03' &&  $scope.eventSiteId!=='si04'){
             $scope.noMap= true;
@@ -25,13 +26,14 @@ angular.module('OGTicketsApp.controllers')
         //segun lo que corresponda abre las opciones para que el usuario haga la escogencia de los asientos.
         //params: noMap: indica si el sitio posee un mapa o no
         $scope.displayPanel= function () {
-            if($scope.noMap){
-                $scope.inputPanel= true;
-                $scope.buttons= true;
-                $scope.resume= true;
-            }else{
-                $scope.showMap= true;
-            }
+
+                if($scope.noMap){
+                    $scope.inputPanel= true;
+                    $scope.buttons= true;
+                    $scope.resume= true;
+                }else{
+                    $scope.showMap= true;
+                }
         };
 
         var reserved=[];
@@ -41,24 +43,15 @@ angular.module('OGTicketsApp.controllers')
         /*params: zoneName, zoneId, rows, cols, site(teatro, auditorio, estadio, palacio), location(diteccion en la qie estan orientadas las butcas)*/
         $scope.ShowSiteSeats= function (zoneName, zoneId, rows, cols, site, location) {
             console.log("zoneName:" + zoneName + " zoneId " + zoneId +  " rows: " + rows + " cols: " + cols + " location: " + location);
-            //$scope.seatsDisplay= true
+            $scope.seatsDisplay= true;
             $scope.rows= seatsService.getRows(rows);
             $scope.cols= seatsService.getCols(cols);
             reserved= seatsService.getReserved(zoneId, site, eventId);
-            console.log(reserved);
-            
+            $scope.showMap= false;
         };
-
-       
-
-       
-
-
-        
 
         // seat onClick
         $scope.seatClicked = function(seatPos) {
-            console.log("Selected Seat: " + seatPos);
             var index = selected.indexOf(seatPos);
             if(index != -1) {
                 // seat already selected, remove
@@ -81,17 +74,37 @@ angular.module('OGTicketsApp.controllers')
         // clear selected
         $scope.clearSelected = function() {
             selected = [];
+            $scope.msg="No ha seleccionado ningún asiento";
+            $scope.total="";
+            $scope.clientSelectedSeats="";
+            $scope.buttons= false;
+            $scope.showPrice= false;
         };
 
         // show selected, esto hay que cambiarlo
         $scope.showSelected = function() {
             if(selected.length > 0) {
-                alert("Selected Seats: \n" + selected);
+                $scope.resume= true;
+                $scope.msg= "Usted tiene " + selected.length + " boletos seleccionados para el evento " + $scope.currentEvent.name + " en el " + $scope.eventSiteName+ ".";
+                $scope.clientSelectedSeats= "Asientos seleccionados " + selected.join(', ')+".";
+                $scope.total=(($scope.currentEvent.ticketsPrice) * selected.length) +".";
+                $scope.showPrice= true;
+                $scope.buttons= true;
             } else {
-                alert("No seats selected!");
+                $scope.msg="No ha seleccionado ningún asiento";
+                $scope.total="";
+                $scope.showPrice= false;
+                $scope.buttons= false;
             }
         };
         //end of seats
+
+        $scope.back= function () {
+            $scope.clearSelected();
+            $scope.showMap= true;
+            $scope.seatsDisplay= false;
+            $scope.buttons= false;
+        };
 
 
 
