0a180e8596c2d4da7cac27aab43b02b75dc38f39
ariaguilar21 <ariaguiuma@hotmail.com>
2016-04-10 01:38:15 -0600

    siteprofile y eventtypeprofile avance

--- logic/controllers/siteProfileController.js
+++ logic/controllers/siteProfileController.js
@@ -1,6 +1,9 @@
 angular.module('OGTicketsApp.controllers')
 .controller('siteProfileController', ['$scope', '$routeParams', 'siteService', function ($scope, $routeParams, siteService) {
 	
+	var siteId = $routeParams.siteId;
 
+	// Sets on "currentSite" the whole event by the id
+    $scope.currentSite = siteService.retrieveSite(siteId);
 
 }]); //end -controller-
