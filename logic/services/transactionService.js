acb2a9f1b3592d14596a758a6f0175b16ab3ae05
ariaguilar21 <ariaguiuma@hotmail.com>
2016-04-15 20:42:04 -0600

    client stuff

--- logic/services/transactionService.js
+++ logic/services/transactionService.js
@@ -1,5 +1,5 @@
 angular.module('OGTicketsApp.services')
-.service('transactionService', ['localStorageService', function(localStorageService) {
+.service('transactionService', ['localStorageService', 'eventService', function(localStorageService, eventService) {
 
 	// Saves credit card into database
     // Param value is the credit card object
@@ -17,6 +17,26 @@ angular.module('OGTicketsApp.services')
     	return "RE-" + idClient + "-" + idEvent + "-" + time;
     };
 
+    var transactions= localStorageService.getAll("transactionList");
+    var events= eventService.eventsList;
+
+    //Retrieves the transactions by client. Params clientID
+
+    var retrieveTransactionsByClient = function (cId){
+        result = transactions.filter(function (item) {
+            return item.idClient == cId;
+        });
+        return result;
+    };
+
+    //Retrieves the events from the transaction eventID. Params TransactionID
+
+    $scope.getEventsFromTransactions = function (transEventId){
+        result = events.filter(function (item) {
+            return item.id == transEventId;
+        });
+        return result;
+    };
     
 
 
@@ -24,6 +44,9 @@ angular.module('OGTicketsApp.services')
 	return{
 	setCreditCard:setCreditCard,
 	generatePurchaseCode:generatePurchaseCode,
-	generateReservationCode:generateReservationCode
+	generateReservationCode:generateReservationCode,
+    transactions:transactions,
+    retrieveTransactionsByClient:retrieveTransactionsByClient,
+    getEventsFromTransactions:getEventsFromTransactions
 	};
 }]);//end -service-
