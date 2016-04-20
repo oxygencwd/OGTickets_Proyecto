77a42be92f529952bef4a4254e6c3f9386f81557
Natalie Mata <natymata@gmail.com>
2016-04-20 14:32:09 -0600

    php devolver los tipos de evento SIN la descripcion, la descripcion falla por cotejamients

---
+++ back-end/app/src/services/EventsService.php
@@ -0,0 +1,76 @@
+<?php
+
+/**
+ * EventsService.php
+ */
+
+namespace App\Services;
+
+class EventsService {
+
+    private $storage;
+    private $validation;
+
+    /**
+     * EventsService constructor.
+     */
+    public function __construct() {
+        $this->storage = new StorageService();
+        $this->validation = new ValidationService();
+    }
+
+
+    public function getAllEventTypes(){
+    	$result=[];
+    	$query= "SELECT idTipoEvento, Nombre, Foto
+				FROM tbtipoevento
+				WHERE Activo= 1";
+		// Query params
+	    $params = [];
+
+	    $getAllResult = $this->storage->query($query, $params);
+
+	    $foundRecords = array_key_exists("meta", $getAllResult) &&
+            $getAllResult["meta"]["count"] > 0;
+
+        if ($foundRecords) {
+            $result["message"] = "Event types found";
+            $eventTypes = $getAllResult["data"];
+
+            foreach ($eventTypes as $type) {
+                $result["data"][] = [
+                	"id" => $type["idTipoEvento"],
+                	"name" => $type["Nombre"],
+                	"description" => $type["Descripcion"],
+                	"image" => $type["Foto"]
+                ];
+            } 
+        } else {
+            $result["message"] = "Event types not found";
+            $result["error"] = true;
+        }
+
+	    return $result;
+
+
+    }//end -getAllEventTypes-
+
+
+
+
+
+
+
+
+
+}//end-class-
+
+
+
+
+
+
+
+
+
+
