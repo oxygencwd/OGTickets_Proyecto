12eeb7389e9a310de942be05c898ef1d3dee7a27
Natalie Mata <natymata@gmail.com>
2016-04-19 19:39:23 -0600

    lista de solicitud de promotres

--- logic/controllers/promoterRequest.js
+++ logic/controllers/promoterRequest.js
@@ -3,9 +3,23 @@ angular.module('OGTicketsApp.controllers')
 	$scope.init = function (){
 		$scope.url = $location.url();
 		$scope.promReq = '/promoter-request';
+		getRequest();
 	};
 	
-	$scope.promPendingCheck = promotorService.promotorsPendingCheck();
+	//$scope.promPendingCheck = promotorService.promotorsPendingCheck()
+	function getRequest() {
+		var promise= promotorService.promotorsPendingCheck();
+		promise.then(function(data) {
+			console.log(data);
+			var jsonList= angular.toJson(data.data);
+			console.log(jsonList);
+			$scope.promPendingCheck= jsonList;
+		})
+		.catch(function(error) {
+			console.log(error);
+		})
+	}
+	
 
 	$scope.init();
 
