4c11a66a4dc21fd7eece59a2893488ab17d58551
ariaguilar21 <ariaguiuma@hotmail.com>
2016-04-23 05:09:00 -0600

    all users

--- logic/controllers/clientProfileController.js
+++ logic/controllers/clientProfileController.js
@@ -4,8 +4,10 @@ angular.module('OGTicketsApp.controllers')
 	var clientId = $routeParams.clientId;
 	var loggedUser= userService.getLoggedUser();
 	var loggedUserId;
-	$scope.client = clientService.retrieveClient(clientId);
-	$scope.transactions = transactionService.retrieveTransactionsByClient(clientId);
+
+	$scope.client = {};
+	
+	// $scope.transactions = transactionService.retrieveTransactionsByClient(clientId);
 
 	if(loggedUser){
 		loggedUserId= loggedUser.id;
@@ -16,13 +18,42 @@ angular.module('OGTicketsApp.controllers')
         $location.path('/client-profile-edit/'+clientId);
     };
 
-    $scope.events = function(){
-		angular.forEach(transactions, function(item) {
-			var events = transactionService.getEventsFromTransactions(item.eventId);
+  //   $scope.events = function(){
+		// angular.forEach(transactions, function(item) {
+		// 	var events = transactionService.getEventsFromTransactions(item.eventId);
 
-		});
+		// });
 
-		return events;
+		// return events;
+  //   };
+
+    $scope.retrieveClient = function (){
+    	var promise = clientService.retrieveClient(clientId);
+		promise.then(function(data) {
+			$scope.client= data.data[0];
+
+
+		})
+		.catch(function(error) {
+			console.log(error);
+		})
+
+
     };
+
+    $scope.getClientEvents = function (){
+    	var promise = clientService.getClientEvents(clientId);
+		promise.then(function(data) {
+			$scope.events= data.data;
+			
+			console.log($scope.events);
+		})
+		.catch(function(error) {
+			console.log(error);
+		})
+    };
+
+    $scope.retrieveClient();
+    $scope.getClientEvents();
 
 }]); //end -controller-
