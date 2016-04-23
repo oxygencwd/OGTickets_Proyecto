90f4fc73601156bc2eae851ce50d3ed72c766e35
David Guillen <maiden8-8@hotmail.com>
2016-04-23 02:55:46 -0600

    Modificaciones

--- logic/controllers/allSitesController.js
+++ logic/controllers/allSitesController.js
@@ -3,10 +3,19 @@ angular.module('OGTicketsApp.controllers')
 	$scope.init = function (){
 		$scope.url = $location.url();
 		$scope.allSites = '/all-sites';
+		getActiveSites();
 	};
 
-
-	$scope.sitesList = siteService.sites;
+	var getActiveSites = function (){
+		var promise = siteService.getSiteList();
+		promise.then(function(data) {
+			$scope.sitesList= data.data;
+			console.log($scope.sitesList);
+		})
+		.catch(function(error) {
+			console.log(error);
+		})
+	};
 
 	$scope.init();
 
